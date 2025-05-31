6f9ec90e062dbd2fd7f0d57c61595c9542307aca
Yu Ling Chen <chenYuLing@hismax.onmicrosoft.com>
2025-05-31 14:34:27 +0800

    refactor: simplify Records component by conditionally rendering messages based on user role

--- src/routes/Records.tsx
+++ src/routes/Records.tsx
@@ -7,16 +7,11 @@ const Records = () => {
 
     const isEditor = role === "editor";
 
-   
-
     return (
         <div className="p-8">
             <h1 className="text-2xl font-bold">紀錄頁</h1>
-
-            {isEditor && <p>你是編輯者，可以新增 / 刪除紀錄</p>}
-            {!isEditor && <p>你是瀏覽者，只能看紀錄</p>}
             <RecordList />
-            <RecordForm />
+            {isEditor && <RecordForm />}
         </div>
     );
 };
